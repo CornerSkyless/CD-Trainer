e24e72272bcaf5a5c3032062d348a48bddd3aa92
cornerskyless <573196853@qq.com>
2018-04-08 22:03:43 +0800

    画图板测试完成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { SignaturePadModule } from 'angular2-signaturepad';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     TabsPage
   ],
   imports: [
-    BrowserModule,
+    BrowserModule,SignaturePadModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SignaturePad } from 'angular2-signaturepad/signature-pad';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,39 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild(SignaturePad) signaturePad: SignaturePad;
+  @ViewChild("MainSection") mainSection: ElementRef;
 
+  private signaturePadOptions = {
+    'minWidth': 3,
+    'canvasWidth': 500,
+    'canvasHeight': 300,
+    'dotSize': 3,
+    'backgroundColor': 'rgb(255,255,255)'
+  };
+
+  ngAfterViewInit() {
+    this.signaturePad.clear();
+    const width = this.mainSection.nativeElement.offsetWidth;
+    const height = this.mainSection.nativeElement.offsetHeight - 170;
+    this.signaturePad.set('canvasWidth',width);
+    this.signaturePad.set('canvasHeight',height);
+    this.signaturePad.set('backgroundColor','rgb(255,255,255)');
+  }
+
+  drawComplete() {
+    console.log(this.signaturePad.toDataURL());
+  }
+
+  drawStart() {
+    console.log('begin drawing');
+  }
+
+  clear() {
+    this.signaturePad.clear();
+  }
+
+  constructor(public navCtrl: NavController) {
   }
 
 }
